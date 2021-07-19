7307aa352a1e3cc8a75a97b3618f56bfb23b741f
Tania Tykhonenko <tania.tihonenko@gmail.com>
2021-07-19 21:04:36 +0300

    rr

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('./views/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/form-submit', function(req, res, next) {
+router.post('./views/form-submit', function(req, res, next) {
   res.render('form-submit', { query: req.body });
 });
 
