a78094b8cfc6c3d227b14aba17c58a82b2a9923f
Tania Tykhonenko <tania.tihonenko@gmail.com>
2021-07-19 23:28:39 +0300

    55

--- app.js
+++ app.js
@@ -11,6 +11,54 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+const vgsForm = window.VGSCollect.create('tntkn4ugi9r', 'sandbox', () => {});
+
+//const css = {
+//  boxSizing: 'border-box',
+//  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI"',
+//};
+
+
+
+vgsForm.field('#cardnumber', {
+  type: 'card-number',
+  name: 'card_number',
+  successColor: '#4F8A10',
+  errorColor: '#D8000C',
+  placeholder: 'Card number',
+  showCardIcon: true,
+  validations: ['required', 'validCardNumber'],
+  css: css,
+});
+
+vgsForm.field('#cvv', {
+  type: 'card-security-code',
+  name: 'card_cvc',
+  successColor: '#4F8A10',
+  errorColor: '#D8000C',
+  placeholder: 'CVC',
+  maxLength: 3,
+  validations: ['required', 'validCardSecurityCode'],
+  css: css,
+});
+
+vgsForm.field('#expdate', {
+  type: 'card-expiration-date',
+  name: 'card_exp',
+  successColor: '#4F8A10',
+  errorColor: '#D8000C',
+  placeholder: 'MM / YY',
+  validations: ['required', 'validCardExpirationDate'],
+  css: css,
+});
+
+form.submit('/post', {}, (status, response) => {
+   console.log(status, response);
+ }, (error) => {
+    console.log(error);
+ });
+
 // load script
 // const collect = await loadVGSCollect({
 // vaultId: 'tntkn4ugi9r',
