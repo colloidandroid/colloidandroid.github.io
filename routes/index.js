e8d0795003544b243ba2e299bd38d61232c9bad8
Tania Tykhonenko <tania.tihonenko@gmail.com>
2021-07-21 00:51:47 +0300

    zz

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('https://tntkn4ugi9r.SANDBOX.verygoodproxy.com/post', function(req, res, next) {
+router.post('/form-submit', function(req, res, next) {
   /* res.render('form-submit', { query: req.body });  */
-    res.redirect(307, '/form-submit' => res.render('form-submit', { query: req.body }));
+    res.redirect(307, 'https://tntkn4ugi9r.SANDBOX.verygoodproxy.com/post' => res.render('/form-submit', { query: req.body }));
 
 });
 
