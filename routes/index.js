b1dc9cb2ab5bc858874a4d807747f91df9b87162
Tania Tykhonenko <tania.tihonenko@gmail.com>
2021-07-21 00:21:37 +0300

    ccc

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/form-submit', function(req, res, next) {
   res.render('form-submit', { query: req.body }); 
+    res.redirect(307, '/form-submit');
+
 });
 
 
